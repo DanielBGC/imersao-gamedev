91f9612205d398668e93fd25e4e5f35e3f611219
Daniel Borba Guimarães da Costa <daniel.borba-@hotmail.com>
2020-06-22 14:57:39 -0300

    Organizando a pasta assets

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
 function setup() {
-    createCanvas(400, 400);
+    //Cria um canvas com tamanho dinâmico (largura e altura da tela)
+    createCanvas(windowWidth, windowHeight);
 }
   
+//Essa função será chamada infinitas vezes
 function draw() {
-    background(220);
+    background(200);
+
+    //circle(x, y, raio)
+    circle(mouseX, mouseY, 20)
 }
